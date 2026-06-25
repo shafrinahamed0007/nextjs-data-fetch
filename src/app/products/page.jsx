e0a075d32946ecab73f6eb713ef8d4d23725e6f2
shafrinahamed0007 <mrdeveloperdevv@gmail.com>
2026-06-25 22:06:41 +0600

    dynamic data fetching cachce no-store and revalidate

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -2,7 +2,7 @@ import Product from "@/Components/Product";
 
 const getProducts = async () => {
   const res = await fetch("http://localhost:3004/products", {
-    cache: "force-cache",
+    next: { revalidate: 20 },
   });
   return res.json();
 };
@@ -12,7 +12,7 @@ const productsPage = async () => {
   return (
     <div className="container mx-auto">
       Total Products: {products.length}
-      <div className="grid md:grid-cols-3 lg:grid-cols-4 gap-5 my-10">
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-5 my-10">
         {products.map((product) => (
           <Product key={product.id} product={product} />
         ))}
